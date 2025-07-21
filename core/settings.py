3bb965d6e7512412c94525fcc06bf62e2913b4c0
mohmdloai <mhmd.00.eng@gmail.com>
2025-07-21 21:33:20 +0300

    Add : AUTH_USER_MODEL in project settings

--- core/settings.py
+++ core/settings.py
@@ -151,3 +151,6 @@ SIMPLE_JWT={
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.UserAccount'
