3132118bea08cd4c7590028168473b2d22d9dfb6
mohmdloai <mhmd.00.eng@gmail.com>
2025-07-21 21:32:34 +0300

    Create : custom users model -> UserAccount and a custom manager for it -> UserAccountManager

--- users/models.py
+++ users/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser ,BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class UserAccountManager(BaseUserManager):
+
+    def create_user(self, email, name, password= None):
+
+        if not email:
+            raise ValueError("email must provided")
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            name= name
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_realtor(self, email, name, password=None):
+        user = self.create_user(email, name, password)
+
+        user.is_realtor = True
+        user.save(using=self._db)
+
+        return user
+
+    def create_super_user(self, email, name, password=None):
+        user = self.create_user(email, name, password)
+
+        user.is_staff = True
+        user.is_superuser=True
+
+        user.save(using=self._db)
+
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=255, unique=True)
+    name  = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = ['name', 'email']
+    USERNAME_FIELD  = 'email'
+
+    is_realtor = models.BooleanField(default=False)
+
+
+    objects = UserAccountManager()
+
+    def __str__(self):
+        return self.email
