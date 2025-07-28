051d177a270a6c499a93bb6fa30ccf2fd45bc484
mohmdloai <mhmd.00.eng@gmail.com>
2025-07-28 20:34:02 +0300

    Define: Listing class

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Listing(models.Model):
+    class SaleType(models.TextChoices):
+        FOR_SALE = 'For Sale'
+        FOR_RENT = 'For Rent'
+
+    class HomeType(models.TextChoices):
+        HOUSE = 'House'
+        CONDO = 'Condo'
+        TOWNHOUSE = 'Townhouse'
+
+
+    realtor = models.EmailField(max_length=100)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField( unique=True)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=30)
+    description = models.TextField()
+    price = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    sale_type = models.CharField(max_length=20, choices=SaleType.choices, default=SaleType.FOR_SALE)
+    home_type = models.CharField(max_length=20, choices=HomeType.choices, default=HomeType.HOUSE)
+
+    main_photo = models.ImageField(upload_to='listings/')
+    photo_1 = models.ImageField(upload_to='listings/')
+    photo_2 = models.ImageField(upload_to='listings/')
+    photo_3 = models.ImageField(upload_to='listings/')
+
+    is_published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return self.title
