7c001a308a37736066126cc531bfad054c2360c7
mohmdloai <mhmd.00.eng@gmail.com>
2025-07-29 04:26:15 +0300

    Create: RegisterView and validate all req body, import all required

--- users/views.py
+++ users/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+User = get_user_model()# which is my custom user model
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
 
-# Create your views here.
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+
+        try:
+
+            data = request.body
+
+            name = data['name']
+            email = data['email']
+            password = data['password']
+            re_password = data['re_password']
+            is_realtor = data['is_realtor']# => it returns a str
+
+
+            if is_realtor == 'True':
+                is_realtor =True
+            else:
+                is_realtor =False
+
+            if password == re_password:
+                if len(password) >=8:
+                    if not User.objects.filter(email=email).exists():
+
+                        if not is_realtor:
+                            User.objects.acreate_user(name=name,email=email,password=password)
+                            return Response(
+                                {'success': 'User created successfully'},
+                                status=status.HTTP_201_CREATED
+                            )
+                        else:
+                            User.objects.create_realtor(name=name,email=email,password=password)
+                            return Response(
+                                {'success': 'Realtor created successfully'},
+                                status=status.HTTP_201_CREATED
+                            )
+
+                    else:
+                        return Response(
+                        {'error': 'User already exists ! '},
+                        status=status.HTTP_400_BAD_REQUEST
+                )
+
+
+
+                else:
+                    return Response(
+                    {'error': 'Password must be at least 8 characters! '},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+
+            else:
+                return Response(
+                    {'error': 'Password don\'t match, Please retry again'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+        except:
+            return Response(
+                {'error':"Oh!, something went wrong while registering"},
+                status= status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
