5100d013fb3ccd2953de0502345fd89a1116a8e2
mohmdloai <mhmd.00.eng@gmail.com>
2025-08-16 22:39:06 +0300

    Fix: leading slash in urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -5,9 +5,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-    path('/api/token/', TokenObtainPairView.as_view()),
-    path('/api/token/refresh/', TokenRefreshView.as_view()),
-    path('/api/token/verify/', TokenVerifyView.as_view()),
-    path('/auth/user/', include('users.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/token/verify/', TokenVerifyView.as_view()),
+    path('auth/user/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
