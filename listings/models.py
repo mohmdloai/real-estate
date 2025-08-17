4eafeaa70c90a1db4e4ec37ee8b92a47cf82271a
mohmdloai <mhmd.00.eng@gmail.com>
2025-08-17 23:55:25 +0300

    Add: bedrooms field

--- listings/models.py
+++ listings/models.py
@@ -22,7 +22,10 @@ class Listing(models.Model):
     zipcode = models.CharField(max_length=30)
     description = models.TextField()
     price = models.IntegerField()
+
     bathrooms = models.DecimalField(max_digits=2, decimal_places=1)
+    bedrooms = models.DecimalField(max_digits=2, decimal_places=1)
+
     sale_type = models.CharField(max_length=20, choices=SaleType.choices, default=SaleType.FOR_SALE)
     home_type = models.CharField(max_length=20, choices=HomeType.choices, default=HomeType.HOUSE)
 
