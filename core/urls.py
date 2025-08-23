55c92c9e9ee70e6e611b5bd18bff9a45c805d502
mohmdloai <mhmd.00.eng@gmail.com>
2025-08-23 23:56:18 +0300

    Include: listings urls

--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/token/verify/', TokenVerifyView.as_view()),
     path('auth/user/', include('users.urls')),
+    path('api/listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]
