1e1f0fbe889c647660ec91a5c7129c5aad695808
mohmdloai <mhmd.00.eng@gmail.com>
2025-08-27 23:20:08 +0300

    Set: an endpoint for ListingView

--- listings/urls.py
+++ listings/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ManagingListingView, ListingDetailView
+from .views import ManagingListingView, ListingDetailView, ListingView
 
 urlpatterns = [
     path('manage' , ManagingListingView.as_view()),
     path('detail' , ListingDetailView .as_view()),
+    path('get-listings' , ListingView .as_view()),
 ]
