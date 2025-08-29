1eab868c412d656ed0e4a664c42ba27027510a25
mohmdloai <mhmd.00.eng@gmail.com>
2025-08-29 21:01:06 +0300

    Add: descriptive comments

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party apps
     'rest_framework',
+    # created apps
     'users',
     'listings',
 ]
