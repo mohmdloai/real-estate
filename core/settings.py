98288592a58a6a9c924ed3b6706640dfef42baec
mohmdloai <mhmd.00.eng@gmail.com>
2025-09-11 11:33:45 +0300

    Update: settings with default db for pytest

--- core/settings.py
+++ core/settings.py
@@ -72,7 +72,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {},
+    'default': {'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',},
     'users': {
         'ENGINE': os.getenv('USERS_DB_ENGINE'),
         'NAME': os.getenv('USERS_DB_NAME'),
