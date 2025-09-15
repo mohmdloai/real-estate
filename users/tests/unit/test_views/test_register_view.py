ea3b8ef04c1e02d005ce4e6192ec337c93b17bde
mohmdloai <mhmd.00.eng@gmail.com>
2025-09-15 07:09:07 +0300

    feat(test_users_views): Add func test_register_user_password_mismatch

--- users/tests/unit/test_views/test_register_view.py
+++ users/tests/unit/test_views/test_register_view.py
@@ -1,16 +1,16 @@
-import pytest
-from django.urls import reverse
-from rest_framework.test import APIClient
-from rest_framework import status
-from users.models import UserAccount
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
 
 User = get_user_model()
+
 
 @pytest.fixture
 def api_client():
     return APIClient()
-
 
 
 # @pytest.mark.django_db
@@ -19,61 +19,83 @@ def api_client():
 
 # Test happy path for user-register
 
-@pytest.mark.django_db(databases=['default', 'users'])
+
+@pytest.mark.django_db(databases=["default", "users"])
 def test_register_regular_user_success(api_client):
-    #ARRANGE
-    url = reverse('user-register')
+    # ARRANGE
+    url = reverse("user-register")
     _data = {
-        'name': 'Test User',
-        'email': 'test_user@gmail.com',
-        'password': 'user_password',
-        're_password': 'user_password',
-        'is_realtor': 'False'
+        "name": "Test User",
+        "email": "test_user@gmail.com",
+        "password": "user_password",
+        "re_password": "user_password",
+        "is_realtor": "False",
     }
 
-    #ACT
+    # ACT
 
-    response = api_client.post(url, data=_data,format='json')
+    response = api_client.post(url, data=_data, format="json")
 
-    #ASSERT
+    # ASSERT
 
     assert response.status_code == status.HTTP_201_CREATED
-    assert 'success' in response.data
-    assert response.data['success'] == 'User created successfully'
-    assert 'email' in response.data
-    assert User.objects.filter(email= 'test_user@gmail.com').exists()
-    user = User.objects.get(email= 'test_user@gmail.com')
+    assert "success" in response.data
+    assert response.data["success"] == "User created successfully"
+    assert "email" in response.data
+    assert User.objects.filter(email="test_user@gmail.com").exists()
+    user = User.objects.get(email="test_user@gmail.com")
     assert user.is_realtor is False
 
     return None
 
 
-@pytest.mark.django_db(databases=['default', 'users'])
+@pytest.mark.django_db(databases=["default", "users"])
 def test_register_realtor_user_success(api_client):
 
-    #ARRANGE
-    url = reverse('user-register')
+    # ARRANGE
+    url = reverse("user-register")
     _data = {
-        'name': 'Test Realtor',
-        'email': 'test_realtor@gmail.com',
-        'password': 'realtor_password',
-        're_password': 'realtor_password',
-        'is_realtor': 'True'
+        "name": "Test Realtor",
+        "email": "test_realtor@gmail.com",
+        "password": "realtor_password",
+        "re_password": "realtor_password",
+        "is_realtor": "True",
     }
 
-    #ACT
+    # ACT
 
-    response = api_client.post(url, data=_data,format='json')
+    response = api_client.post(url, data=_data, format="json")
 
-    #ASSERT
+    # ASSERT
 
     assert response.status_code == status.HTTP_201_CREATED
-    assert 'success' in response.data
-    assert response.data['success'] == 'Realtor created successfully'
+    assert "success" in response.data
+    assert response.data["success"] == "Realtor created successfully"
 
     # assert 'email' in response.data
-    assert User.objects.filter(email= 'test_realtor@gmail.com').exists()
-    user = User.objects.get(email= 'test_realtor@gmail.com')
+    assert User.objects.filter(email="test_realtor@gmail.com").exists()
+    user = User.objects.get(email="test_realtor@gmail.com")
     assert user.is_realtor is True
 
     return None
+
+
+@pytest.mark.django_db(databases=["default", "users"])
+def test_register_user_password_mismatch(api_client):
+    # Arrange
+    url = reverse("user-register")
+    _data = {
+        "name": "Test User",
+        "email": "test_user@gmail.com",
+        "password": "user_password",
+        "re_password": "not_same_password",
+        "is_realtor": "False",
+    }
+
+    # ACT
+    response = api_client.post(url, data=_data, format="json")
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert "error" in response.data
+    assert response.data["error"] == "Password don't match, Please retry again"
+    assert not User.objects.filter(email="test_user@gmail.com").exists()
